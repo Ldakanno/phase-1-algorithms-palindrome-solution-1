e7a414393a4b75d6e6b7063f1ea948a1f40b1801
Lianne Dakanno <liannedakanno@gmail.com>
2022-07-30 22:14:03 -0500

    commit tree fe77d9a9566a1a73d0ec034779e2fe1ec1c97074

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function reverse(word) {
+  return word.split('').reverse().join('');
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  
+  return word === reversedWord; 
+
 }
 
 /* 
